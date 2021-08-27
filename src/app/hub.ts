118648e2dc52cb7e514b476b60c3a131e1dd93d5
otienonick <nick.odongi@student.moringaschool.com>
2021-08-27 18:37:51 +0300

    added homepage class

---
+++ src/app/hub.ts
@@ -0,0 +1,12 @@
+export class Hub {
+
+    constructor(public login:string,
+        public avatar_url:any,
+         public bio:string,
+         public company:string,
+         public followers:number,
+         public following:number, 
+         public public_repos:number
+         ){}
+
+}
