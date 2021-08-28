52a2956a4cd50172efe4cd8da22b39a015122018
otienonick <nick.odongi@student.moringaschool.com>
2021-08-28 17:57:22 +0300

    added notfound routing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NotfoundComponent } from './notfound/notfound.component';
-import { SearchusersComponent } from './searchusers/searchusers.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'username' , component:HomepageComponent},
-  {path:'search' , component:SearchusersComponent},
   { path: '', redirectTo:"/username", pathMatch:"full"},
   {path: '**',component:NotfoundComponent}
 
